6efa5b943e351f7818b752529ef0c17818ae7ead
alievaisun <alievaaisun@gmail.com>
2023-08-17 00:08:05 +0400

    days of week

---
+++ days of week/app.js
@@ -0,0 +1,27 @@
+let daysOfWeek = +prompt(`write the number between 1 and 7`)
+switch (daysOfWeek) {
+    case 1:
+        console.log("Today is Monday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+    case 3:
+        console.log("Today is Wednesday");
+        break;
+    case 4:
+        console.log("Today is Thursday");
+        break;
+    case 5:
+        console.log("Today is Friday");
+        break;
+    case 6:
+        console.log("Today is Saturday");
+        break;
+    case 7:
+        console.log("Today is Sunday");
+        break;
+    default:
+        console.log("There is no such day");
+        
+}
