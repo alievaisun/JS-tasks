88c2039afd022cbb6dc3b0f8a14712d9e78cf3f9
alievaisun <alievaaisun@gmail.com>
2023-08-16 23:53:29 +0400

    multiplication table

---
+++ multiplication table/app.js
@@ -0,0 +1,7 @@
+let multiplicand = +prompt(`write multiplicand`)
+let multiplier = +prompt(` write multiplier`)
+let product = 0
+for (let product = 0; product>=0; product++) {
+    product= multiplicand*multiplier 
+    console.log(`product is ${product}`);
+}
